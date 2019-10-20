b6dcc789f63a0076d00464f7c1b67eb253e59f71
Supratik Majumdar <supratikmajumdar94@gmail.com>
2019-10-20 22:17:19 +0530

    Cluster model added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,5 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from .mapping import CloudRegion, InstanceType, Status, Tag
+from .cluster import Cluster
 
-__all__ = ["CloudRegion", "Tag", "InstanceType", "Status"]
+__all__ = ["CloudRegion", "Tag", "InstanceType", "Status", "Cluster"]
---
+++ app/models/cluster/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Supratik Majumdar"
+__status__ = "Development"
+
+from .Cluster import Cluster
+
+__all__ = ["Cluster"]
