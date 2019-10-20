c0e96a7f11c3a2aea13c857ebc2d276c5275295d
Supratik Majumdar <supratikmajumdar94@gmail.com>
2019-10-20 22:00:24 +0530

    mapping tables models added

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Supratik Majumdar"
+__status__ = "Development"
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+from .mapping import CloudRegion, InstanceType, Status, Tag
+
+__all__ = ["CloudRegion", "Tag", "InstanceType", "Status"]
