47848295dd89c11bc32e2ed1bb1e13fc802fe054
Supratik Majumdar <supratikmajumdar94@gmail.com>
2019-10-21 21:16:09 +0530

    statuses endpoint working copy commit

--- app/v0/resources/__init__.py
+++ app/v0/resources/__init__.py
@@ -2,5 +2,6 @@ __author__ = "Supratik Majumdar"
 __status__ = "Development"
 
 from .CloudRegion import CloudRegion
+from .Status import Status
 
-__all__ = ["CloudRegion"]
+__all__ = ["CloudRegion", "Status"]
--- app/v0/routes.py
+++ app/v0/routes.py
@@ -4,9 +4,10 @@ __status__ = "Development"
 from flask import Blueprint
 from flask_restful import Api
 
-from .resources import CloudRegion
+from .resources import CloudRegion, Status
 
 v0_blueprint = Blueprint(name="v0", import_name=__name__)
 v0_api = Api(app=v0_blueprint, prefix="/v0")
 
 v0_api.add_resource(CloudRegion, "/cloud_regions/")
+v0_api.add_resource(Status, "/statuses/")
